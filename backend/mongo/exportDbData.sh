0540cd4026edfc339053e2bf3b8aa8a9133e5928
Luca Fehmel <lf054@hdm-stuttgart.de>
2021-05-08 17:01:06 +0200

    finally fixed db data loading

--- backend/mongo/exportDbData.sh
+++ backend/mongo/exportDbData.sh
@@ -1,4 +1,3 @@
-# docker exec mongodb /bin/sh -c "mongoexport -u root -p password --authenticationDatabase admin --db studifix -c cities --out /data/export/newdbexport.json && docker cp mongodb:/data/export/ ."
 docker exec mongodb /bin/sh -c "mongodump --authenticationDatabase admin -d studifix -o data/export/ -u root -p password"
-# docker cp mongodb:/data/export/ .
+docker cp mongodb:/data/export/ .
 
