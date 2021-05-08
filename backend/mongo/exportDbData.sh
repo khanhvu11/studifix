adf4a46e045bcf97ea350689344bd8dcdd3d28be
Luca Fehmel <lf054@hdm-stuttgart.de>
2021-05-08 17:10:02 +0200

    add new collections

--- backend/mongo/exportDbData.sh
+++ backend/mongo/exportDbData.sh
@@ -1,3 +1,3 @@
-docker exec mongodb /bin/sh -c "mongodump --authenticationDatabase admin -d studifix -o data/export/ -u root -p password"
-docker cp mongodb:/data/export/ .
+docker exec mongodb /bin/sh -c "mongodump --authenticationDatabase admin -d studifix -o export/ -u root -p password"
+docker cp mongodb:data/export/studifix ./mongo/restore
 
