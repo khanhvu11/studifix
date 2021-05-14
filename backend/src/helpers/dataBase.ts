bdc8098ed6e25e97ab7d4a5d074ccfc35f2392fa
Luca Fehmel <lf054@hdm-stuttgart.de>
2021-05-14 15:52:33 +0200

    adjust filterquery Generator

--- backend/src/helpers/dataBase.ts
+++ backend/src/helpers/dataBase.ts
@@ -16,21 +16,24 @@ export const scholarshipFilterqueryGenerator = (idLists: IUserInputList): any =>
 
     const { commitment, occupation, requirement, semester, graduation, nationality, support, course, state, religion } = idLists;
 
-    commitment ? tempArray.push({ $or: [{ commitment: { $in: refactorIDs(commitment) } }, { commitment: null }] }) : null;
-    occupation ? tempArray.push({ $or: [{ occupation: { $in: refactorIDs(occupation) } }, { occupation: null }] }) : null;
-    graduation ? tempArray.push({ $or: [{ graduation: { $in: refactorIDs(graduation) } }, { graduation: null }] }) : null;
-    support ? tempArray.push({ $or: [{ support: { $in: refactorIDs(support) } }, { support: null }] }) : null;
-    course ? tempArray.push({ $or: [{ course: { $in: refactorIDs(course) } }, { course: null }] }) : null;
-    state ? tempArray.push({ $or: [{ state: { $in: refactorIDs(state) } }, { state: null }] }) : null;
-    religion ? tempArray.push({ $or: [{ religion: { $in: refactorIDs(religion) } }, { religion: null }] }) : null;
-    nationality ? tempArray.push({ $or: [{ nationality: { $in: refactorIDs(nationality) } }, { nationality: null }] }) : null;
-    requirement ? tempArray.push({ $or: [{ requirement: { $in: refactorIDs(requirement) } }, { requirement: null }] }) : null;
+    commitment ? tempArray.push({ $or: [{ 'commitment.value': { $in: refactorIDs(commitment) } }, { 'commitment.value': null }] }) : null;
+    occupation ? tempArray.push({ $or: [{ 'occupation.value': { $in: refactorIDs(occupation) } }, { 'occupation.value': null }] }) : null;
+    graduation ? tempArray.push({ $or: [{ 'graduation.value': { $in: refactorIDs(graduation) } }, { 'graduation.value': null }] }) : null;
+    support ? tempArray.push({ $or: [{ 'support.value': { $in: refactorIDs(support) } }, { 'support.value': null }] }) : null;
+    course ? tempArray.push({ $or: [{ 'course.value': { $in: refactorIDs(course) } }, { 'course.value': null }] }) : null;
+    state ? tempArray.push({ $or: [{ 'state.value': { $in: refactorIDs(state) } }, { 'state.value': null }] }) : null;
+    religion ? tempArray.push({ $or: [{ 'religion.value': { $in: refactorIDs(religion) } }, { 'religion.value': null }] }) : null;
+    nationality ? tempArray.push({ $or: [{ 'nationality.value': { $in: refactorIDs(nationality) } }, { 'nationality.value': null }] }) : null;
+    requirement ? tempArray.push({ $or: [{ 'requirement.value': { $in: refactorIDs(requirement) } }, { 'requirement.value': null }] }) : null;
 
-    semester ? tempArray.push({ $or: [{ semester }, { semester: null }] }) : null;
+    semester ? tempArray.push({ $or: [{ 'semester.value': semester }, { 'semester.value': null }] }) : null;
 
     if (tempArray.length === 0) {
         return {};
     }
+
+    console.log('jo', tempArray);
+
     return { $and: tempArray };
 };
 
