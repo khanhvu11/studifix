69a222f13677c8cd941d982f8c9fa4643cd77cd4
Luca Fehmel <lf054@hdm-stuttgart.de>
2021-06-22 21:52:56 +0200

    adjust send application path to go back to filterpage

--- web/src/ApplyingProcess/ApplyingMain/ApplyingMain.js
+++ web/src/ApplyingProcess/ApplyingMain/ApplyingMain.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import personalInfo from '../personalInfo'
+import personalInfo from '../personalInfo';
 
 //import TextArea from './TextArea'
 import './Filter.css';
@@ -11,35 +11,31 @@ import groupObject from './groupData';
 import Error from '../InputComponents/Error';
 /* import Dropdown from './Dropdown'; */
 
-
 function Filter(props) {
   const URL = process.env.REACT_APP_API_URL_PREFIX || 'http://localhost';
   const [result, setResult] = useState({});
   const history = useHistory();
-  const [error, setError] = useState('')
-
+  const [error, setError] = useState('');
 
   var getNextkey = (currentKey) => {
-    var existKeys = [...props.groupNameList, 'submit']
+    var existKeys = [...props.groupNameList, 'submit'];
     /* labels.filter((key) =>
       key !== null && !removedKeyList.includes(key) ? key : null
     ); */
-   /*  existKeys.push('submit'); */
+    /*  existKeys.push('submit'); */
     var ind = existKeys.indexOf(currentKey);
     props.setGroupName(existKeys[ind + 1]);
   };
 
   var getALlResult = (key, value) => {
+    result[key] = value;
 
-      result[key] = value;
-  
     setResult(result);
   };
 
-  console.log(result)
+  console.log(result);
 
   var toReview = () => {
-
     fetch(URL + `/api/data/apply/${props.scholarshipId}`, {
       method: 'POST',
       body: JSON.stringify(result),
@@ -58,35 +54,37 @@ function Filter(props) {
           return Promise.reject(error);
         }
         history.push({
-          pathname: '/review',
-          state: {scholarships : data},
+          pathname: '/',
+          state: { scholarships: data },
         });
         /* response.json() */
       })
       /* .then(json => setScholarships(json)) */
       .catch((error) => {
-        setError(error)
-        console.error('There was an error!', error)
-    });
-  }
+        setError(error);
+        console.error('There was an error!', error);
+      });
+  };
 
   return (
     <div className="filter">
-      {(error)? <Error error = {error}/>:null}
+      {error ? <Error error={error} /> : null}
       <div className="options">
         {Object.keys(groupObject).map((groupName, id) => {
           var memberList = groupObject[groupName];
           return (
-            props.data && <InputGroup
-              memberList = {memberList}
-              groupName={groupName}
-              currentGrN = {props.groupName} 
-              setGroupName={props._setGroupName} 
-              lang={props.lang} 
-              data = {props.data}
-              getValue = {getALlResult}
-            />
-          )
+            props.data && (
+              <InputGroup
+                memberList={memberList}
+                groupName={groupName}
+                currentGrN={props.groupName}
+                setGroupName={props._setGroupName}
+                lang={props.lang}
+                data={props.data}
+                getValue={getALlResult}
+              />
+            )
+          );
         })}
         <button
           type="button"
