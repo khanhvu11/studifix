65193e5c38de71f9a1826d4764be86e6be7d342d
Luca Fehmel <lf054@hdm-stuttgart.de>
2021-07-10 22:13:33 +0200

    add new tests for token generation and hashing of user passwords

--- api/src/helpers/token.ts
+++ api/src/helpers/token.ts
@@ -3,15 +3,15 @@ var jwt = require('jsonwebtoken');
 const SECRET = 'mySecretPassword';
 const EXPIRATION_TIME_HOURS: number = 24;
 
-const signToken = (_id: string): Promise<any> => {
+export const signToken = (_id: string, secret?: string, expirationTime?: string): Promise<any> => {
     return new Promise(async (resolve, reject) => {
         try {
             const token = jwt.sign(
                 {
                     data: _id
                 },
-                SECRET,
-                { expiresIn: `${EXPIRATION_TIME_HOURS}h` }
+                secret ? secret : SECRET,
+                { expiresIn: expirationTime ? `${expirationTime}h` : `${EXPIRATION_TIME_HOURS}h` }
             );
             resolve(token);
         } catch (error) {
@@ -20,17 +20,13 @@ const signToken = (_id: string): Promise<any> => {
     });
 };
 
-const verifyToken = (token: string): Promise<any> => {
+export const verifyToken = (token: string, secret?: string): Promise<any> => {
     return new Promise(async (resolve, reject) => {
         try {
-            resolve(jwt.verify(token, SECRET));
+            const userID = jwt.verify(token, secret ? secret : SECRET);
+            resolve(userID);
         } catch (error) {
             reject(error);
         }
     });
-};
-
-export default {
-    verifyToken,
-    signToken
 };
--- api/src/middlewares/validation.ts
+++ api/src/middlewares/validation.ts
@@ -1,18 +1,18 @@
-import { Request, Response, NextFunction } from 'express'
-import tokens from '../helpers/tokens'
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from '../helpers/token';
 
 const validateUserByToken = async (req: Request, res: Response, next: NextFunction) => {
+    let token: string = req.headers.authorization!;
 
-    let token: string = req.headers.authorization!
-
-    await tokens.verifyToken( token ).then(result => {
-        res.locals.userID = result.data
-        next();
-    })
-    .catch(error => {
-        return res.status(401).json({
-            message: error.message
+    await verifyToken(token)
+        .then((result) => {
+            res.locals.userID = result.data;
+            next();
         })
-    })
-}
-export default { validateUserByToken }
+        .catch((error) => {
+            return res.status(401).json({
+                message: error.message
+            });
+        });
+};
+export default { validateUserByToken };
